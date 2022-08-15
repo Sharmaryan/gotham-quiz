d020ef1dab5473cd7cb49a64eb76dc93181f2092
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-08-15 11:23:57 +0530

    styles : Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 import './Navbar.css';
 export const Navbar = () => {
     return (
-        <div>Navbar </div>
+        <div className='navbar'> 
+        <div className="navbar__title">gotham quiz</div>
+            <div className="navbar_theme"><i className="fa fa-sun-o"></i></div>
+        </div>
     )
 }
