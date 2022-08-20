bb265a8661022fd912647c9d7a54ef4a4a5f9000
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-08-20 19:13:27 +0530

    styles : rules page

--- src/pages/Rules/Rules.tsx
+++ src/pages/Rules/Rules.tsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom';
+import { useQuiz } from '../../context/quiz-context';
 import './Rules.css';
 export const Rules = () => {
+  const { currentQuizId } = useQuiz();
+
   return (
     <div className='rules'>
-        <h1 className="rules__heading">Rules</h1>
-        <ol className="rules__descriptons">
-          <li className="rules__description">Do not cheat.</li>  
-          <li className="rules__description">Don't forget rules number one.</li>  
-        </ol>
-        <button className="rules__btn">Start</button>
+      <h1 className="rules__heading">Rules</h1>
+      <ol className="rules__descriptons">
+        <li className="rules__description">Do not cheat.</li>
+        <li className="rules__description">Don't forget rules number one.</li>
+      </ol>
+      <Link to={`/quiz/${currentQuizId}`} className="rules__btn">Start</Link>
     </div>
   )
 }
