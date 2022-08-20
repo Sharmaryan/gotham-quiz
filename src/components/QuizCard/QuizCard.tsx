723d3b869746ce42286d7e26cd14c21b511f30dc
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-08-20 19:12:35 +0530

    styles : quiz card

--- src/components/QuizCard/QuizCard.tsx
+++ src/components/QuizCard/QuizCard.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { quizOne, quizTwo } from "../../data";
 import './QuizCard.css';
+import { useQuiz } from "../../context/quiz-context";
 
 export const QuizCard = () => {
     const navigate = useNavigate();
+    const { setCurrentQuizId } = useQuiz();
 
-    const playQuizHandler = () => {
+    const playQuizHandler = (id: string) => {
         navigate('/rules');
+        setCurrentQuizId(id);
     }
 
     return (
@@ -15,7 +18,7 @@ export const QuizCard = () => {
                 return <div key={quiz.id} className='card'>
                     <img src={quiz.quizImage} alt='' className="card__image" />
                     <p className="card__title">{quiz.quizName}</p>
-                    <button className="card__btn" onClick={playQuizHandler}>play now</button>
+                    <button className="card__btn" onClick={() => playQuizHandler(quiz.id)}>play now</button>
                 </div>
             })}
         </div>
